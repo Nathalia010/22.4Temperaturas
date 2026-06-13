eb860c14715cf469fa22cee2528077dc87f0f7a7
KarenMartinez <k.nathalia.martinez.b@gmail.com>
2026-06-13 12:48:07 -0500

    v1 html

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
-let tem_celsios = parseFloat(prompt("Ingrese temperatura en celsius:"));
-
-    
-let rKelvin = (tem_celsios +273.15);
-let rFahrenhit = (tem_celsios *1.8)+32
-
 console.log(`Grados Kelvin: ${rKelvin}`);
 console.log(`Grados Fahrenheit: ${rFahrenhit}`);
 
+document.getElementById("btn-calcular").addEventListener("click", () => {
+    
+    
+    const inputElemento = document.getElementById("num-cel");
+    const tem_celsius = parseFloat(inputElemento.value);
+    //let tem_celsios = parseFloat(prompt("Ingrese temperatura en celsius:"));
+
+
+    
+    let rKelvin = tem_celsius + 273.15;
+    let rFahrenheit = (tem_celsius * 1.8) + 32;
+
+    console.log(`Grados Kelvin: ${rKelvin}`);
+    console.log(`Grados Fahrenheit: ${rFahrenheit}`);
+
+    
+    const cajaResultado = document.getElementById("resultado");
+    cajaResultado.innerHTML = `Kelvin: ${rKelvin}K <br> Fahrenheit: ${rFahrenheit}°F`;
+});
